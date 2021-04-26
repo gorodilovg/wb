5ae332564c0ae42c59257416a87159a42f303cf3
Герман Городилов <tyske@yahoo.com>
2021-04-26 22:50:36 +0500

    Finished

--- wb/api.py
+++ wb/api.py
@@ -2,14 +2,13 @@ import math
 import uuid
 import datetime
 from urllib.parse import urljoin
-from json.decoder import JSONDecodeError
-import itertools
 
 import requests
-import dateutil.parser
+import pandas as pd
 
 
 FBS_ORDERS_BASE_API = 'https://suppliers-orders.wildberries.ru'
+FBS_ORDERS_STATUSES_BASE_API = 'https://marketplace-remotewh.wildberries.ru'
 STATISTICS_BASE_API = 'https://suppliers-stats.wildberries.ru'
 CONTENT_BASE_API = 'https://suppliers-api.wildberries.ru'
 ORDERS_API_TZ = "+03:00"
@@ -40,34 +39,58 @@ def _get_product_list_request_data(supplier_id, offset, limit):
         }
     }
 
+
 def check_connection(access):
     """Проверка работы
-        /card/list/ API-эндпоинта suppliers-api.wildberries.ru и
-        /api/v1/supplier/orders API-эндпоинта suppliers-stats.wildberries.ru.
+        /card/list/ API-эндпоинта suppliers-api.wildberries.ru и,
+        /api/v1/supplier/orders API-эндпоинта suppliers-stats.wildberries.ru и,
+        /api/v1/supplier/reportDetailByPeriod API-эндпоинта https://suppliers-orders.wildberries.ru и
+        /api/public/v1/supply_tasks/status API-эндпоинта https://marketplace-remotewh.wildberries.ru.
     """
     session = _get_session_for_content_api(authorization_token=access['CONTENT_API_AUTHORIZATION_TOKEN'])
-
     content_api_response = session.post(
         url=urljoin(CONTENT_BASE_API, '/card/list'),
-        json=_get_product_list_request_data(supplier_id=access['SUPPLIER_ID'], offset=0, limit=1))
-    statistics_api_response = requests.get(
-        url=urljoin(STATISTICS_BASE_API, '/api/v1/supplier/orders'),
+        json=_get_product_list_request_data(
+            supplier_id=access['SUPPLIER_ID'], offset=0, limit=1)
+    )
+    fbs_orders_api_response = requests.get(
+        url=urljoin(FBS_ORDERS_BASE_API, '/api/v1/orders'),
         params={
-            'key': access['API_KEY'],
-            'dateFrom': datetime.datetime.now()
+            'date_start': datetime.datetime.now().isoformat() + ORDERS_API_TZ,  # приведение времени к таймзоне WB
+            'date_end': datetime.datetime.now().isoformat() + ORDERS_API_TZ
+        },
+        headers={'X-Auth-Token': access['ORDERS_API_TOKEN']}
+    )
+    fbs_orders_statuses_api_response = requests.get(
+        url=urljoin(FBS_ORDERS_STATUSES_BASE_API, '/api/public/v1/supply_tasks/status'),
+        params={
+            'date_start': datetime.datetime.now().isoformat() + ORDERS_API_TZ,  # приведение времени к таймзоне WB
+            'date_end': datetime.datetime.now().isoformat() + ORDERS_API_TZ
+        },
+        headers={'X-Auth-Token': access['ORDERS_API_TOKEN']}
+    )
+    sales_api_response = requests.get(
+        url=urljoin(STATISTICS_BASE_API, '/api/v1/supplier/reportDetailByPeriod'),
+        params={
+            'key': access['STATISTICS_API_KEY'],
+            'dateFrom': datetime.datetime.now().isoformat(),
+            'dateTo': datetime.datetime.now().isoformat()
         }
     )
 
     return (content_api_response.status_code
-            and statistics_api_response.status_code == requests.codes.ok)
+            and fbs_orders_api_response.status_code
+            and fbs_orders_statuses_api_response.status_code
+            and sales_api_response.status_code == requests.codes.ok)
 
 
 def product_list(access, page_size=50):
     """Получение генератора с "сырыми" данными товаров в формате JSON.
        Каждый вызов функции next() возвращает 1 объект.
             Аргументы:
-                page_size (int): Максимальное количество карточек товаров, которые надо вывести.
-
+                access (dict): Словарь с данными для авторизации в API.
+                page_size (int): Максимальное количество карточек товаров,
+                                 которые надо вывести.
             Возвращает:
                 product (json): Генератор.
     """
@@ -93,7 +116,8 @@ def product_list(access, page_size=50):
         response_data = session.post(
             url=urljoin(CONTENT_BASE_API, '/card/list'),
             json=_get_product_list_request_data(
-                supplier_id=access['SUPPLIER_ID'], offset=page * page_size, limit=page_size)
+                supplier_id=access['SUPPLIER_ID'],
+                offset=page * page_size, limit=page_size)
         ).json()
 
         if not 'result' in response_data:
@@ -107,8 +131,8 @@ def product_list(access, page_size=50):
 def fbs_order_list(access, from_datetime, to_datetime):
     """Получение генератора с "сырыми" данными заказов поставщика в формате JSON.
        Каждый вызов функции next() возвращает 1 объект.
-
-            Параметры:
+            Аргументы:
+                access (dict): Словарь с данными для авторизации в API.
                 from_datetime (datetime): Конкретная дата с которой выгружать заказы.
                 to_datetime (datetime): Конкретная дата по которую выгружать заказы.
             Возвращает:
@@ -117,7 +141,7 @@ def fbs_order_list(access, from_datetime, to_datetime):
     response = requests.get(
         url=urljoin(FBS_ORDERS_BASE_API, '/api/v1/orders'),
         params={
-            'date_start': from_datetime.isoformat() + ORDERS_API_TZ,
+            'date_start': from_datetime.isoformat() + ORDERS_API_TZ,  # приведение времени к таймзоне WB
             'date_end': to_datetime.isoformat() + ORDERS_API_TZ
         },
         headers={'X-Auth-Token': access['ORDERS_API_TOKEN']}
@@ -129,13 +153,22 @@ def fbs_order_list(access, from_datetime, to_datetime):
     response_data = response.json()
 
     for order in response_data:
+        order['rid'] = int(order['items'][0]['rid'])
         yield order
     else:
         yield from ()
 
 
 def statistics_sales_list(access, from_datetime):
-
+    """Получение генератора с "сырыми" данными продаж в формате JSON.
+       Каждый вызов функции next() возвращает 1 объект.
+       Выгружает продажи с переданной даты по сегодняшний день.
+            Параметры:
+                access (dict): Словарь с данными для авторизации в API.
+                from_datetime (datetime): Конкретная дата с которой выгружать продажи.
+            Возвращает:
+                order (json): Генератор.
+    """
     response = requests.get(
         url=urljoin(STATISTICS_BASE_API, '/api/v1/supplier/reportDetailByPeriod'),
         params={
@@ -156,44 +189,54 @@ def statistics_sales_list(access, from_datetime):
         yield from ()
 
 
-# order
-# {
-#     "date_created": "2021-04-10T08:49:56.713175+03:00",
-#     "items": [
-#       {
-#         "rid": "50418284378"
-#          ...
-#       },
-#       {
-#         "rid": "60318284378"
-#          ...
-#       },
-# }
+def fbs_orders_statuses_list(access, from_datetime, to_datetime):
+    response = requests.get(
+        url=urljoin(FBS_ORDERS_STATUSES_BASE_API, '/api/public/v1/supply_tasks/status'),
+        params={
+            'date_start': from_datetime.isoformat() + ORDERS_API_TZ,  # приведение времени к таймзоне WB
+            'date_end': to_datetime.isoformat() + ORDERS_API_TZ
+        },
+        headers={'X-Auth-Token': access['ORDERS_API_TOKEN']}
+    )
 
-# sale
-# {
-#     "rid": 50406899506
-# }
+    if response.status_code != requests.codes.ok:
+        yield from ()
 
-# В заказе может быть несколько вещей
-# Если вещей несколько - то для каждой вещи должна быть одна продажа
-# На основе продаж сформировать список заказов
-# Наверное каждой вещи соответсвует одна продажа
+    response_data = response.json()
+
+    for order in response_data:
+        order = {
+            'order_id': order['order_id'],
+            'last_status': order['items'][-1]['status']
+        }
+        yield order
+    else:
+        yield from ()
 
 
 def order_list(access, from_datetime, to_datetime):
-    orders = fbs_order_list(access=access, from_datetime=from_datetime, to_datetime=to_datetime)
-    sales = list(statistics_sales_list(access=access, from_datetime=from_datetime))
+    """Получение генератора с "сырыми" данными заказов,
+       сопоставленных с продажими и статусами заказов в формате JSON.
+       Каждый вызов функции next() возвращает 1 объект.
+            Параметры:
+                access (dict): Словарь с данными для авторизации в API.
+                from_datetime (datetime): Конкретная дата с которой выгружать продажи.
+                to_datetime (datetime): Конкретная дата по которую выгружать заказы.
+            Возвращает:
+                order (json): Генератор.
+    """
+    orders_df = pd.DataFrame.from_records(
+        fbs_order_list(access=access, from_datetime=from_datetime, to_datetime=to_datetime)
+    )
+    sales_df = pd.DataFrame.from_records(
+        statistics_sales_list(access=access, from_datetime=from_datetime)
+    )
+    orders_statuses_df = pd.DataFrame.from_records(
+        fbs_orders_statuses_list(access=access, from_datetime=from_datetime,
+                                 to_datetime=to_datetime)
+    )
+    orders = pd.merge(orders_df, sales_df, on='rid').merge(
+        orders_statuses_df, on='order_id').to_dict(orient='records')
 
-    orders_rids = [
-        int(item["rid"]) for item in itertools.chain.from_iterable(
-            order["items"] for order in orders)
-    ]
-
-    sales_rids = [sale["rid"] for sale in sales]
-
-    existing_sales_rids = [sales_rid for sales_rid in sales_rids if sales_rid in orders_rids]
-
-    for sale in sales:
-        if sale["rid"] in existing_sales_rids:
-            yield sale
+    for order in orders:
+        yield order
--- wb/test_api.py
+++ wb/test_api.py
@@ -1,6 +1,5 @@
 import datetime
 import time
-import itertools
 
 import dateutil.parser
 
@@ -8,7 +7,7 @@ from wb.consts import (
     CONTENT_API_AUTHORIZATION_TOKEN, SUPPLIER_ID,
     STATISTICS_API_KEY, ORDERS_API_TOKEN
 )
-from wb.api import check_connection, product_list, fbs_order_list, statistics_sales_list, order_list
+from wb.api import check_connection, product_list, order_list
 
 
 ACCESS = {
@@ -27,72 +26,20 @@ def test_get_products_list_returns_cards():
     assert list(products), "функция должна вернуть не пустой список товаров"
 
 
-def test_order_list_returns_true_date_range_orders():
-    time.sleep(60)
+def test_order_list():
     from_datetime = datetime.datetime(year=2021, month=4, day=1)
-    to_datetime = datetime.datetime(year=2021, month=4, day=23)
-    orders = list(
-        sorted(
-            fbs_order_list(access=ACCESS, from_datetime=from_datetime, to_datetime=to_datetime),
-            key=lambda order: dateutil.parser.parse(order["date"])
-        )
+    to_datetime = datetime.datetime(year=2021, month=4, day=25)
+    orders = order_list(ACCESS, from_datetime, to_datetime)
+    assert list(orders), "функция должна вернуть не пустой список заказов"
+
+
+from_datetime = datetime.datetime(year=2021, month=4, day=17)
+to_datetime = datetime.datetime(year=2021, month=4, day=19, hour=23, minute=59)
+orders = list(
+    sorted(
+        order_list(ACCESS, from_datetime, to_datetime),
+        key=lambda order: dateutil.parser.parse(order["date_created"])
     )
-
-    first_order_datetime = dateutil.parser.parse(orders[0]["date"])
-    last_order_datetime = dateutil.parser.parse(orders[-1]["date"])
-
-    assert first_order_datetime < last_order_datetime
-    assert last_order_datetime > first_order_datetime
-    assert first_order_datetime >= from_datetime
-    assert last_order_datetime <= to_datetime
-
-
-from_datetime = datetime.datetime(year=2021, month=4, day=1)
-to_datetime = datetime.datetime(year=2021, month=4, day=25)
-
-# orders = fbs_order_list(access=ACCESS, from_datetime=from_datetime, to_datetime=to_datetime)
-#
-# orders = list(map(
-#         lambda order: {
-#             "date_created": order["date_created"],
-#             "items": [item["rid"] for item in order["items"]]
-#         },
-#         sorted(
-#             orders,
-#             key=lambda order: dateutil.parser.parse(order["date_created"])
-#         )
-#     )
-# )
-# orders_rids = list(map(int, itertools.chain.from_iterable(order["items"] for order in orders)))
-#
-# sales = statistics_sales_list(ACCESS, from_datetime=from_datetime)
-#
-# sales = list(map(
-#         lambda sale: {
-#             "order_dt": sale["order_dt"],
-#             "sale_dt": sale["sale_dt"],
-#             "rid": sale["rid"]
-#         },
-#         sorted(
-#             sales,
-#             key=lambda sale: dateutil.parser.parse(sale["order_dt"])
-#         )
-#     )
-# )
-# sales_rids = list(sale["rid"] for sale in sales)
-#
-# matches = []
-#
-# for sale in sales_rids:
-#     if sale in orders_rids:
-#         matches.append(sale)
-#
-#
-# # orders = fbs_order_list(access=ACCESS, from_datetime=from_datetime, to_datetime=to_datetime)
-# # sales = statistics_sales_list(access=ACCESS, from_datetime=from_datetime)
-
-
-orders = list(order_list(ACCESS, from_datetime, to_datetime))
-
+)
 
 print()
